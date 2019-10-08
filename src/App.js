657e7ba8753c222a77c73c3b03042f16e03386f4
Leonardo Borchartt <leob_b@hotmail.com>
2019-10-08 15:37:43 -0300

    send

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import './App.css';
 
 import logo from '../src/assets/logo.svg';
-/* <div className="container">
-<img src={logo} alt="AirCnC" />
-<div className="content">
-<Routes />
-</div>
-</div>  */
 
 import Routes from './routes';
 
 function App() {
   return (
-    <p></p>
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
   );
 }
 
