01bf96c4608b4fab401219c3a8fa2bdc8b999732
Leonardo Borchartt <leob_b@hotmail.com>
2019-10-08 15:08:07 -0300

    send

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import './App.css';
 
 import logo from '../src/assets/logo.svg';
+/* <div className="container">
+<img src={logo} alt="AirCnC" />
+<div className="content">
+<Routes />
+</div>
+</div>  */
 
 import Routes from './routes';
 
 function App() {
-  return (
-    <div className="container">
-      <img src={logo} alt="AirCnC" />
-      <div className="content">
-        <Routes />
-      </div>
-    </div>
-  );
+  return 
 }
 
 export default App;
